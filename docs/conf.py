1b14114655b2c0d34550d61b562deb6694b81193
Rui Rua <rui.rrua@gmail.com>
2026-04-23 08:22:29 -0300

    fix conf

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,8 @@ templates_path = ["_templates"]
 exclude_patterns = ["_build", ".venv", "Thumbs.db", ".DS_Store"]
 
 # HTML output
-html_baseurl = "https://sanadlab.github.io/MORAl/"
+import os
+html_baseurl = os.environ.get("SPHINX_HTML_BASEURL", "")
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_theme_options = {
